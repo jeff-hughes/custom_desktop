d681c5a029f2ad037c80b3213d5e030e67782f71
Jeff Hughes <jeff.hughes@gmail.com>
2019-10-03 20:13:01 -0400

    distribute.sh references its own directory rather than the PWD of your terminal

--- distribute.sh
+++ distribute.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cp ./.xinitrc ~/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cp ${DIR}/.xinitrc ~/
 
 mkdir -pv ~/.config/openbox
-cp ./openbox/* ~/.config/openbox/
+cp ${DIR}/openbox/* ~/.config/openbox/
 
 mkdir -pv ~/.config/tint2
-cp ./tint2/tint2rc ~/.config/tint2/
+cp ${DIR}/tint2/tint2rc ~/.config/tint2/
